5797645c983d5dca123976412c341a9707625e52
Aman Kumar <aman.kmr4@yahoo.com>
2020-09-22 22:56:21 +1000

    I have got the API working with the webpage

--- script.js
+++ script.js
@@ -2,25 +2,65 @@
 var citytxt = $("#citytxt");
 var cityButton = $("#citybtn");
 var newCitiesArea = $(".cityList")
-var cityarray =  []
+var cityarray = []
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //now lets try to create a onclick fucntion that will add that value to our buuton below
-$("button").on("click", function(event) {
+$(cityButton).on("click", function (event) {
     event.preventDefault();
     // here we have got the text within the input 
-var citySeacrhtxt = $("input").val();
-console.log(citySeacrhtxt);
+    var citySeacrhtxt = $("input").val();
+    console.log(citySeacrhtxt);
+    cityarray.push(citySeacrhtxt);
+    console.log(cityarray);
 
-var newBtn = $("<button>").text(citySeacrhtxt);
-newBtn.addClass("col-12");
+    createButton();
 
-newCitiesArea.append(newBtn);
-
-citytxt.val("");
-
-
-
-
-
+    citytxt.val("");
 });
 
+//lets create a function that will create new buttons
+function createButton() {
+    newCitiesArea.empty();
+    cityarray.forEach(function (city) {
+        var newBtn = $("<button>").text(city);
+        newBtn.addClass("col-12 cityButton");
+        newBtn.attr("data-name", city);
+
+        newCitiesArea.append(newBtn);
+    }
+    )
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// lets focus on the city list buttons to call the API
+function currentWeather(cityclicked) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityclicked + "&units=imperial&appid=4591a3428058e369dab3f9b2d3ba83e8";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+        var cityName = response.name;
+        var temperature = response.main.temp;
+        var humidity = response.main.humidity;
+        var windSpeed = response.wind.speed
+
+        console.log(cityName);
+        console.log(temperature);
+        console.log(humidity);
+        console.log(windSpeed);
+
+
+    });
+
+}
+
+function displayWeather() {
+    var cityclicked = $(this).attr("data-name");
+    currentWeather(cityclicked);
+}
+
+// now lets run a onclick function on the buttons on the citylist
+$(document).on("click", ".cityButton", displayWeather);
